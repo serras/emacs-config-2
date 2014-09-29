0c6484e0a79e097f74a89d4d8f4873300d2200ca
Alejandro Serrano <trupill@gmail.com>
2014-09-29 10:37:46 +0200

    Try

--- haskell.el
+++ haskell.el
@@ -5,14 +5,18 @@
        (concat (file-name-directory (or load-file-name
                                     (buffer-file-name)))
                "packages/haskell-mode"))
-(add-to-list 'load-path
-       (concat (file-name-directory (or load-file-name
-                                    (buffer-file-name)))
-               "packages/ghc-mod/elisp"))
+;(add-to-list 'load-path
+;       (concat (file-name-directory (or load-file-name
+;                                    (buffer-file-name)))
+;               "packages/ghc-mod/elisp"))
 (add-to-list 'load-path
        (concat (file-name-directory (or load-file-name
                                     (buffer-file-name)))
                "packages/company-ghc"))
+(add-to-list 'load-path
+       (concat (file-name-directory (or load-file-name
+                                    (buffer-file-name)))
+               "packages/hi2"))
 
 ; Set up environment
 (setenv "PATH" (concat "/opt/ghc/7.8.3/bin:" (getenv "PATH")))
@@ -29,15 +33,15 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; SHM
 ;(add-hook 'haskell-mode-hook 'structured-haskell-mode)
+(require 'hi2)
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
 
 ; Add F8 key combination for going to imports block
-(add-hook 'haskell-mode-hook
-          (lambda () (define-key haskell-mode-map [f8] 'haskell-navigate-imports)))
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
 
 ; Set up hasktags
 (custom-set-variables '(haskell-tags-on-save t))
-(add-hook 'haskell-mode-hook
-          (lambda () (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)))
 
 ; Set up interactive mode
 ; (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
@@ -46,19 +50,24 @@
 (custom-set-variables '(haskell-process-type 'cabal-repl))
 
 ; Add key combinations for haskell-mode
-(add-hook 'haskell-mode-hook (lambda () 
+(eval-after-load 'haskell-mode '(progn
   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
   (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
   (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
   (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
   (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
   (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
-(add-hook 'cabal-mode-hook (lambda () 
-  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-ode-clear)
+(eval-after-load 'haskell-cabal '(progn 
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
   (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
   (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile))
+(eval-after-load 'haskell-cabal
+  '(define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile))
 
 ; Set up ghc-mod
 (autoload 'ghc-init "ghc" nil t)
